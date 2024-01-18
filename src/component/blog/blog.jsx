6f37635ad07b10b76c071aaf99005a6aa8c11f3e
NGUENOUz <wilfrieddzomeu90@gmail.com>
2024-01-18 17:58:00 +0100

    dd

--- src/component/blog/blog.jsx
+++ src/component/blog/blog.jsx
@@ -20,7 +20,7 @@ function Blog() {
                Entrez dans mon univers pour mieux me découvrir 
             </div>
           </nav>
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmonnouveauguide%2F&tabs=timeline&width=360&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="360" height="500" style={{border:"none", display:"flex",flexWrap:"wrap"}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmonnouveauguide%2F&tabs=timeline&width=320&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="360" height="500" style={{border:"none", display:"flex",flexWrap:"wrap"}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
         <Footer/>
       </div>
